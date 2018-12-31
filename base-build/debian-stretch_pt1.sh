3dffc97c8c95cfc72885f0b268812a75fda94e23
fithwum <34775371+fithwum@users.noreply.github.com>
2018-12-31 14:39:41 -0800

    Update debian-stretch_pt1.sh

--- base-build/debian-stretch_pt1.sh
+++ base-build/debian-stretch_pt1.sh
@@ -5,12 +5,14 @@
 RELEASE=stable
 
 echo "Getting needed things"
+wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt3.sh
 apt-get install -y debootstrap ftp-upload bash dirmngr
 sleep 1
 echo "Downloading distro of choice"
 debootstrap --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg --force-check-gpg --variant=minbase --components=main,contrib,non-free --include=dirmngr,apt-transport-https,wget,bzip2 --arch=amd64 stable /debian-stretch http://deb.debian.org/debian/
 sleep 1
 du --human-readable --summarize debian-stretch
+wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt2.sh /debian-stretch/debian-stretch_pt2.sh
 sleep 10
 echo "Mounting folders for root"
 mount --bind /dev debian-stretch/dev
