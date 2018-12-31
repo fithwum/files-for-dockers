e2860ac0bbba0354df4f2a2b7c0d7b86d29c3633
fithwum <34775371+fithwum@users.noreply.github.com>
2018-12-31 14:31:27 -0800

    Create debian-stretch_pt2.sh

---
+++ base-build/debian-stretch_pt2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright (c) 2018 fithwum
+# All rights reserved
+
+echo "APT::Get::Assume-Yes \"true\";" | tee /etc/apt/apt.conf.d/10-assume_yes
+sleep 2
+apt-get install --no-install-recommends git
+sleep 2
+apt-get remove --allow-remove-essential e2fsprogs e2fslibs pinentry-curses whiptail kmod iptables iproute2 dmidecode
+sleep 2
+apt-get clean
+sleep 2
+find /var/lib/apt/lists/ -maxdepth 2 -type f -delete
+sleep 2
+exit
