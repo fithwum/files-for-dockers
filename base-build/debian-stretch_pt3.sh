463bc5e34b6b7c3f0949b842ae30778493928186
fithwum <34775371+fithwum@users.noreply.github.com>
2018-12-31 14:36:22 -0800

    Update debian-stretch_pt3.sh

--- base-build/debian-stretch_pt3.sh
+++ base-build/debian-stretch_pt3.sh
@@ -2,15 +2,21 @@
 # Copyright (c) 2018 fithwum
 # All rights reserved
 
+RELEASE=
+
 umount debian-stretch/dev/pts
 umount debian-stretch/dev
 umount debian-stretch/proc
 umount debian-stretch/sys
 sleep 2
 du --human-readable --summarize debian-stretch
-sleep 2
+sleep 10
 tar --verbose --create --file debian-stretch.tar --directory debian-stretch .
+sleep 1
+du --human-readable --summarize debian-stretch
 sleep 2
-sudo tar -cvjf debian-stretch.tar.bz2 debian-stretch
+tar -cvjf debian-stretch.tar.bz2 debian-stretch
+sleep 1
+du --human-readable --summarize debian-stretch
 sleep 2
-ftp-upload -v -h 192.168.1.92:8001 -u {} --password {} -d /files debian-stretch.tar
+ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar
