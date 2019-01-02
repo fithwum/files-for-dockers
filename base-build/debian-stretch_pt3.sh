8fc272d55a9210ef34c006a06ae9f279ae37e5f9
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-02 11:37:41 -0800

    Update debian-stretch_pt3.sh

--- base-build/debian-stretch_pt3.sh
+++ base-build/debian-stretch_pt3.sh
@@ -14,17 +14,11 @@ umount debian-stretch/proc
 sleep 1
 umount debian-stretch/sys
 echo "cleaning up unneeded script"
-rm -frv ./debian-stretch_pt2.sh
+rm -frv /debian-stretch/debian-stretch_pt2.sh
 sleep 1
 echo "Filesystem size after."
 sleep 1
 du --human-readable --summarize debian-stretch
-sleep 10
-tar --verbose --create --file debian-stretch.tar --directory debian-stretch .
-sleep 1
-echo "Filesystem bundle."
-sleep 1
-du --human-readable --summarize debian-stretch.tar
 sleep 10
 tar -cvjf debian-stretch.tar.bz2 --directory debian-stretch .
 sleep 1
@@ -35,7 +29,5 @@ sleep 10
 echo "upload to ftp."
 sleep 1
 ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar
-sleep 1
-ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar.bz2
 sleep 1
 exit
