ff79dd59af3012867606590abbc27cda66aba077
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-01 22:38:20 -0800

    Update debian-stretch_pt3.sh

--- base-build/debian-stretch_pt3.sh
+++ base-build/debian-stretch_pt3.sh
@@ -4,19 +4,36 @@
 
 RELEASE=
 
+echo "Unmounting folders for root"
+sleep 1
 umount debian-stretch/dev/pts
+sleep 1
 umount debian-stretch/dev
+sleep 1
 umount debian-stretch/proc
+sleep 1
 umount debian-stretch/sys
-sleep 2
+sleep 1
+echo "Filesystem size after."
+sleep 1
 du --human-readable --summarize debian-stretch
 sleep 10
 tar --verbose --create --file debian-stretch.tar --directory debian-stretch .
 sleep 1
-du --human-readable --summarize debian-stretch
-sleep 2
-tar -cvjf debian-stretch.tar.bz2 debian-stretch
+echo "Filesystem bundle."
 sleep 1
 du --human-readable --summarize debian-stretch
-sleep 2
+sleep 10
+tar -cvjf debian-stretch.tar.bz2 --directory debian-stretch .
+sleep 1
+echo "Filesystem archive."
+sleep 1
+du --human-readable --summarize debian-stretch
+sleep 10
+echo "upload to ftp."
+sleep 1
 ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar
+sleep 1
+ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar.bz2
+sleep 1
+exit
