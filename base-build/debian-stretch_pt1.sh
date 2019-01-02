a9fbb1bd83d8c4ecccb891612807e43dadb822db
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-01 22:26:02 -0800

    Update debian-stretch_pt1.sh

--- base-build/debian-stretch_pt1.sh
+++ base-build/debian-stretch_pt1.sh
@@ -8,23 +8,27 @@ echo "Getting needed things"
 wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt2.sh -O /debian-stretch_pt2.sh
 wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt3.sh -O /debian-stretch_pt3.sh
 chmod +x debian-stretch_pt2.sh debian-stretch_pt3.sh
-
+sleep 1
 apt-get install -y debootstrap ftp-upload bash dirmngr
 sleep 1
 echo "Downloading distro of choice"
-debootstrap --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg --force-check-gpg --variant=minbase --components=main,contrib,non-free --include=dirmngr,apt-transport-https,wget,bzip2 --arch=amd64 stable /debian-stretch http://deb.debian.org/debian/
+debootstrap --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg --force-check-gpg --variant=minbase --components=main,contrib,non-free --include=dirmngr,apt-transport-https,wget,bzip2,bash,nano --arch=amd64 stable /debian-stretch http://deb.debian.org/debian/
 sleep 1
 du --human-readable --summarize debian-stretch
 sleep 10
 echo "Mounting folders for root"
+sleep 1
 mount --bind /dev debian-stretch/dev
-sleep 0.5
+sleep 1
 mount --bind /dev/pts debian-stretch/dev
-sleep 0.5
+sleep 1
 mount --bind /proc debian-stretch/proc
-sleep 0.5
+sleep 1
 mount --bind /sys debian-stretch/sys
-sleep 5
+sleep 1
+cp -v debian-stretch_pt2.sh /debian-stretch
+sleep 1
 echo "Changine to new root"
+sleep 1
 sudo chroot debian-stretch
 exit
