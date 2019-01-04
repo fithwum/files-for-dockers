83d25fee78ad8c1ca6c4621289d7f46d868bf89f
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-04 00:09:29 -0800

    Update debian-stretch_pt1.sh

--- base-build/debian-stretch_pt1.sh
+++ base-build/debian-stretch_pt1.sh
@@ -3,7 +3,8 @@
 # All rights reserved
 
 RELEASE=stable
-
+echo "getting system updates"
+apt-get -y update
 echo "Getting needed things"
 wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt2.sh -O /debian-stretch_pt2.sh
 wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/base-build/debian-stretch_pt3.sh -O /debian-stretch_pt3.sh
@@ -11,11 +12,11 @@ chmod +x debian-stretch_pt2.sh debian-stretch_pt3.sh
 sleep 1
 apt-get install -y debootstrap ftp-upload bash dirmngr
 sleep 1
-echo "Downloading distro of choice"
+echo "Downloading debian & selected packages"
 sleep 1
 debootstrap --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg --force-check-gpg --variant=minbase --components=main,contrib,non-free --include=dirmngr,apt-transport-https,wget,bzip2,bash,nano,ca-certificates --arch=amd64 stable /debian-stretch http://deb.debian.org/debian/
 sleep 1
-echo "Filesystem size before."
+echo "Filesystem size uncompressed."
 sleep 1
 du --human-readable --summarize debian-stretch
 sleep 10
