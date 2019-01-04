00d3006ea34ac44c7051481fc2fd0a5dbe1ca394
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-04 10:24:43 -0800

    Update debian-stretch_pt2.sh

--- base-build/debian-stretch_pt2.sh
+++ base-build/debian-stretch_pt2.sh
@@ -4,22 +4,20 @@
 
 echo "APT::Get::Assume-Yes \"true\";" | tee /etc/apt/apt.conf.d/10-assume_yes
 sleep 1
-echo "INFO ! setting certs for wget"
-sleep 1
 # echo "Installing git."
 # sleep 1
 # apt-get install --no-install-recommends git
 # sleep 1
+echo " "
 echo "INFO ! Removeing unnecessary packages."
-sleep 1
 apt-get remove --allow-remove-essential e2fsprogs e2fslibs pinentry-curses whiptail kmod iptables iproute2 dmidecode
 sleep 1
+echo " "
 echo "INFO ! Cleanup"
-sleep 1
 apt-get clean
 sleep 1
 find /var/lib/apt/lists/ -maxdepth 2 -type f -delete
 sleep 1
+echo " "
 echo "INFO ! Type "exit" and reboot for final steps."
-sleep 1
 exit
