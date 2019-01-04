833aacabd79b38ddbdf015c619d6ea5de1b4c155
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-04 10:31:43 -0800

    Update debian-stretch_pt2.sh

--- base-build/debian-stretch_pt2.sh
+++ base-build/debian-stretch_pt2.sh
@@ -2,12 +2,9 @@
 # Copyright (c) 2018 fithwum
 # All rights reserved
 
+echo " "
 echo "APT::Get::Assume-Yes \"true\";" | tee /etc/apt/apt.conf.d/10-assume_yes
 sleep 1
-# echo "Installing git."
-# sleep 1
-# apt-get install --no-install-recommends git
-# sleep 1
 echo " "
 echo "INFO ! Removeing unnecessary packages."
 apt-get remove --allow-remove-essential e2fsprogs e2fslibs pinentry-curses whiptail kmod iptables iproute2 dmidecode
@@ -15,7 +12,6 @@ sleep 1
 echo " "
 echo "INFO ! Cleanup"
 apt-get clean
-sleep 1
 find /var/lib/apt/lists/ -maxdepth 2 -type f -delete
 sleep 1
 echo " "
