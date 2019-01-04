32a3d3c72630fdb3852d0603709494e9ba568fca
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-04 10:24:45 -0800

    Update debian-stretch_pt3.sh

--- base-build/debian-stretch_pt3.sh
+++ base-build/debian-stretch_pt3.sh
@@ -7,16 +7,17 @@ RELEASE=
 echo "INFO ! Cleaning up pt2 of script from base image script"
 rm -frv /debian-stretch/debian-stretch_pt2.sh
 sleep 1
+echo " "
 echo "INFO ! Filesystem size after."
-sleep 1
 du --human-readable --summarize debian-stretch
 sleep 10
 tar -cvjf debian-stretch.tar.bz2 --directory debian-stretch .
 sleep 1
+echo " "
 echo "INFO ! Filesystem archive."
-sleep 1
 du --human-readable --summarize debian-stretch.tar.bz2
 sleep 10
+echo " "
 echo "INFO ! Upload image to ftp."
 sleep 1
 ftp-upload -v -h {IP}:{PORT} -u {USER} --password {PASSWORD} -d /files debian-stretch.tar
