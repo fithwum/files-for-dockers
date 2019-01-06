a3b277157141774847349ef3b7d6421506e879e7
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-06 12:36:08 -0800

    Update debian-stretch_pt2.sh

--- base-build/debian-stretch_pt2.sh
+++ base-build/debian-stretch_pt2.sh
@@ -6,10 +6,6 @@ echo " "
 echo "APT::Get::Assume-Yes \"true\";" | tee /etc/apt/apt.conf.d/10-assume_yes
 sleep 1
 echo " "
-echo "INFO ! Checking for missing updates."
-apt-get update
-sleep 1
-echo " "
 echo "INFO ! Removeing unnecessary packages."
 apt-get remove --allow-remove-essential e2fsprogs e2fslibs pinentry-curses whiptail kmod iptables iproute2 dmidecode
 sleep 1
