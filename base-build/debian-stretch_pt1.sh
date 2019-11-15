9809ffb430d540b362fc39504461a8d79dc02190
fithwum <34775371+fithwum@users.noreply.github.com>
2019-11-15 10:08:18 -0800

    Update debian-stretch_pt1.sh

--- base-build/debian-stretch_pt1.sh
+++ base-build/debian-stretch_pt1.sh
@@ -38,9 +38,6 @@ echo "INFO ! Installing debootstrap,ftp-upload,bash,dirmngr."
 apt-get install -y debootstrap ftp-upload bash dirmngr
 sleep 1
 echo " "
-apt-key --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg adv --keyserver pgpkeys.mit.edu --recv-keys DCC9EFBF77E11517
-sleep 1
-echo " "
 echo "INFO ! Downloading debian & selected packages."
 debootstrap --keyring /etc/apt/trusted.gpg.d/debian-archive-stretch-stable.gpg --force-check-gpg --variant=minbase --components=main,contrib,non-free --include=dirmngr,apt-transport-https,wget,bzip2,bash,nano,ca-certificates,software-properties-common --arch=amd64 stable /debian-stretch http://deb.debian.org/debian/
 echo " "
