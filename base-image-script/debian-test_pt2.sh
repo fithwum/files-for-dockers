6218c7c9808dcc0d470e327d93cea4807dfcfecb
fithwum <fithwum@hotmail.com>
2025-07-04 08:29:20 -0700

    add docker and node

--- base-image-script/debian-test_pt2.sh
+++ base-image-script/debian-test_pt2.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 echo "[CHROOT] Configuring Debian system..."
-
 echo 'APT::Get::Assume-Yes "true";' > /etc/apt/apt.conf.d/10-assume_yes
 
 apt-get update
 apt-get upgrade
 
-apt-get install software-properties-common bash wget curl nano python3 python3-pip python3-venv locales node npm docker
+echo "[CHROOT] Installing Node.js..."
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install nodejs
+
+echo "[CHROOT] Installing Docker..."
+apt-get install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install software-properties-common bash wget curl nano python3 python3-pip python3-venv locales
 
 dpkg -l | grep python3-cryptography || echo "[OK] Not installed"
-
-sleep 30
 
 echo "[CHROOT] Removing unnecessary packages..."
 apt-get remove --purge --allow-remove-essential pinentry-curses whiptail kmod iptables iproute2 dmidecode || true
